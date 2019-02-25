760156f784e950aab950690cf8207b88e48b90e2
Eric Heiden <eric@eric-heiden.de>
2019-02-24 22:18:43 -0800

    Improve collision checking

--- experiments/benchmark_corridors.cpp
+++ experiments/benchmark_corridors.cpp
@@ -11,28 +11,20 @@ namespace og = ompl::geometric;
 int main(int argc, char **argv) {
   Log::instantiateRun();
 
-  for (unsigned int i = 0; i < 2; ++i) {
+  for (unsigned int i = 0; i < 1; ++i) {
     delete global::settings.environment;
     global::settings.environment =
-        GridMaze::createRandomCorridor(50, 50, 4, 30, i + 1);
+        GridMaze::createRandomCorridor(150, 150, 4, 130, i + 1);
 
     global::settings.max_planning_time = 3;
-    global::settings.steer.steering_type = Steering::STEER_TYPE_POSQ;
-    global::settings.steer.posq.alpha = 1.5;
-    global::settings.steer.posq.phi = 0.1;
-    global::settings.steer.posq.rho = 1.2;
-    global::settings.steer.posq.rho_end_condition = 0.1;
-    global::settings.steer.posq.v = 1;
-    global::settings.steer.posq.v_max = 2;
-    global::settings.steer.posq.dt = 0.1;
-    global::settings.steer.posq.axis_length = 0.2;
+    global::settings.steer.steering_type = Steering::STEER_TYPE_REEDS_SHEPP;
     global::settings.steer.initializeSteering();
 
     auto info = nlohmann::json({{"plans", {}}});
     global::settings.environment->to_json(info["environment"]);
 
     //    PathEvaluation::evaluate<ThetaStar>(info);
-    PathEvaluation::evaluate<RRTPlanner>(info);
+    //    PathEvaluation::evaluate<BITstarPlanner>(info);
     PathEvaluation::evaluate<RRTstarPlanner>(info);
     PathEvaluation::evaluate<RRTsharpPlanner>(info);
     //    PathEvaluation::evaluate<InformedRRTstarPlanner>(info);
