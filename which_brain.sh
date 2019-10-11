0b4a9d8a7b1d77cf8412d5a72790d24a2f68ae89
Eric Heiden <eric-heiden@outlook.com>
2019-10-11 18:48:33 +0000

    Add helper function to identify which brain server is used

---
+++ which_brain.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+hn=$(hostname)
+
+if [ $hn == "28401227d07c" ]
+then
+    echo "brain1"
+elif [ $hn == "8078cee05e73" ]
+then
+    echo "brain2"
+elif [ $hn == "d43c614fb8cf" ]
+then
+    echo "brain3"
+else
+    echo "unknown"
+fi
